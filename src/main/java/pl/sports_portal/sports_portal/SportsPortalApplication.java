4736fd01b80963bff5950858d93dd3296f7b8159
lewandowski <lewandowski2004@o2.pl>
2022-11-11 13:20:18 +0100

    Start of the project

---
+++ src/main/java/pl/sports_portal/sports_portal/SportsPortalApplication.java
@@ -0,0 +1,13 @@
+package pl.sports_portal.sports_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sports_portal/sports_portal/SportsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sports_portal.sports_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
